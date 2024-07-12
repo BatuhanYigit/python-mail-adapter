51962a723d1ae5ca2137252a55bf6bb90a2bd367
BatuhanYigit <batuhanyigit34@gmail.com>
2024-07-12 17:32:21 +0300

    Update
    Delete old codes

--- main.py
+++ main.py
@@ -22,31 +22,34 @@ sender_email = os.getenv("SENDER_EMAIL")
 subject_keyword = os.getenv("SUBJECT_KEYWORD")
 
 for item in inbox.filter(subject__contains=subject_keyword):
-    if isinstance(item, Message) and item.sender.email_address == sender_email:
-        print(
-            f"İtem Detayları: {{'subject': '{item.subject}', 'sender': '{item.sender.email_address}', 'body': '{item.body}'}}"
-        )
+    print(
+        f"İtem Detayları: {{'subject': '{item.subject}', 'sender': '{item.sender.email_address}', 'body': '{item.body}'}}"
+    )
+    # if isinstance(item, Message) and item.sender.email_address == sender_email:
+    #     print(
+    #         f"İtem Detayları: {{'subject': '{item.subject}', 'sender': '{item.sender.email_address}', 'body': '{item.body}'}}"
+    #     )
 
-        match = re.search(
-            r"https://analyserschedoutbound\.blob\.core\.windows\.net/[^\s]+\.zip",
-            item.body,
-        )
-        if match:
-            link = match.group(0)
-            print(f"Link found: {link}")
+    #     match = re.search(
+    #         r"https://analyserschedoutbound\.blob\.core\.windows\.net/[^\s]+\.zip",
+    #         item.body,
+    #     )
+    #     if match:
+    #         link = match.group(0)
+    #         print(f"Link found: {link}")
 
-            # SSL SERTİFİKA DEVRE DIŞI BIRAK
-            response = requests.get(link, verify=False)
-            filename = link.split("/")[-1]
-            with open(filename, "wb") as f:
-                f.write(response.content)
-            print(f"File {filename} successfully downloaded.")
-        else:
-            print("No link found in the email body.")
-    else:
-        print(
-            f"Email from {item.sender.email_address} does not match the specified sender."
-        )
+    #         # SSL SERTİFİKA DEVRE DIŞI BIRAK
+    #         response = requests.get(link, verify=False)
+    #         filename = link.split("/")[-1]
+    #         with open(filename, "wb") as f:
+    #             f.write(response.content)
+    #         print(f"File {filename} successfully downloaded.")
+    #     else:
+    #         print("No link found in the email body.")
+    # else:
+    #     print(
+    #         f"Email from {item.sender.email_address} does not match the specified sender."
+    #     )
 
     # Sertifika indirme
     #     if match:
